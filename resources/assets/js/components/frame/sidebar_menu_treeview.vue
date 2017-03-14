f4d982fee304f3e1011108009a1ab30bf612dea2
zhichen.dai <dzcdzcdzcdzc@gmail.com>
2017-03-14 09:26:10 +0800

    修复多级菜单的高亮

--- resources/assets/js/components/frame/sidebar_menu_treeview.vue
+++ resources/assets/js/components/frame/sidebar_menu_treeview.vue
@@ -1,12 +1,12 @@
 <template>
     <ul class="treeview-menu" style="display: none;">
         <template v-for="menu in treeview">
-        <li :class="{active:(menu.menu == currentMenu)}">
+        <li :class="{active:(!currentMenu.indexOf(menu.menu))}">
             <a :href="menu.path"><i class="fa" :class="menu.icons">
         </i>{{menu.display_name}}<span v-if="Object.keys(menu.children).length" class="pull-right-container">
             <i class="fa fa-angle-left pull-right"></i></span>
         </a>
-            <sidebar_menu_treeview v-if="Object.keys(menu.children).length" :treeview="treeview.children"></sidebar_menu_treeview>
+            <sidebar_menu_treeview v-if="Object.keys(menu.children).length" :treeview="menu.children"></sidebar_menu_treeview>
         </li>
         </template>
     </ul>
