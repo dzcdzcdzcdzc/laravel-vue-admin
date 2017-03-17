704bb54980068b323763a18894dcc159b862ed0a
zhichen.dai <dzcdzcdzcdzc@gmail.com>
2017-03-17 17:44:25 +0800

    修改忽略文件

---
+++ public/js/last.js
@@ -0,0 +1,19 @@
+// FIXME:用vue-router的router-link
+$("#app").on("click", "a", function () {
+    var self = $(this);
+    var href = self.attr("href");
+    switch (true) {
+        case !href:
+            event.preventDefault();
+            break;
+        case !(href.toLowerCase().indexOf("javascript")):
+        case !(href.toLowerCase().indexOf("mailto")):
+        case self.attr("target") == "_blank":
+        case self.attr("download"):
+            break;
+        default:
+            router.push(href);
+            event.preventDefault();
+            break;
+    }
+});
