0f0fd10ca7521a615fa73fe135a9b6fef3b35ae9
zhichen.dai <dzcdzcdzcdzc@gmail.com>
2017-04-12 10:04:31 +0800

    封装input控件

--- resources/assets/js/components/forms/input.vue
+++ resources/assets/js/components/forms/input.vue
@@ -1,13 +1,41 @@
 <template>
-    <div class="form-group">
-        <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
-        <div class="col-sm-10">
-            <input type="text" class="form-control" id="inputEmail3" placeholder="Email">
+    <form class="form-horizontal">
+        <div class="box-body">
+            <div class="form-group">
+                <label :for="id" class="col-sm-2 control-label">{{placeholder}}</label>
+                <div class="col-sm-10">
+                    <input :type="type" class="form-control"
+                           :name="name" :id="id" :placeholder="placeholder" :value="value">
+                </div>
+            </div>
         </div>
-    </div>
+    </form>
 </template>
 
 <script>
     export default {
+        props: {
+            'name': {
+                type: String,
+                default: 'name'
+            },
+            'type': {
+                type: String,
+                default: 'text'
+            },
+            'placeholder': {
+                type: String,
+                default: ''
+            },
+            'value': {
+                type: String,
+                default: ''
+            },
+        },
+        data() {
+            return {
+                id: mkid()
+            }
+        },
     }
 </script>
--- resources/assets/js/views/users/edit.vue
+++ resources/assets/js/views/users/edit.vue
@@ -6,11 +6,16 @@
                     <div class="box-header">
                         <h3 class="box-title">用户编辑</h3>
                     </div>
-                    <!-- /.box-header -->
                     <div class="box-body">
-                        <d-input></d-input>
+                        <d-input name="name" placeholder="用户名"></d-input>
+                        <d-input name="email" placeholder="email"></d-input>
+                        <d-input type="password" name="password1" placeholder="密码"></d-input>
+                        <d-input type="password" name="password2" placeholder="再次输入密码"></d-input>
                     </div>
-                    <!-- /.box-body -->
+                    <div class="box-footer">
+                        <button type="submit" class="btn btn-default pull-right">取消</button>
+                        <button type="submit" class="btn btn-info pull-right">提交</button>
+                    </div>
                 </div>
                 <!-- /.box -->
             </div>
